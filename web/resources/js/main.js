686f327a9b6cf158140e79185c7defbe257cbb1d
Marc Porto <marc.porto@hotmail.com>
2016-11-15 11:12:36 +0100

    sql Setup

--- web/resources/js/main.js
+++ web/resources/js/main.js
@@ -19,30 +19,55 @@ $(function() {
     	onPlainPasswordchange(random);
     });
     
+    $('.showPassword').click(function() {
+    	var id = $(this).data('id');
+    	var enc = $(this).siblings('.hiddenAccPassword').val();
+    	var master = sessionStorage.getItem('master');
+    	var decrypted = decryption(enc, master);
+    	$(this).siblings('.outputPassword').val(decrypted);
+    	$(this).css('visibility','hidden');
+    });    
+    
     var onPlainPasswordchange = function(plain) {
     	var masterPassword = sessionStorage.getItem('master');
-    	var encrypted = encryption(plain, masterPassword);
-    	$('.encryptedPassword').val(encrypted);
+    	var obj = encryption(plain, masterPassword);
+    	$('.encryptedPassword').val(obj.enc);
     }
     
-    //*** Wrapper ***//
-    
-    var encryption = function(message, key) {
-    	return CryptoJS.AES.encrypt(message, key);
-    }
-    var decryption = function(enc, key) {
-    	
-    }
-    var hash1 = function(message) {
-    	return CryptoJS.SHA256("Message");
-    }
-    var hash2 = function(message) {
-    	return CryptoJS.SHA512("Message");
-    }
-    var validateHash = function(message, expected) {
-    	
-    }
-    var generateRandom = function() {
-    	return CryptoJS.lib.WordArray.random(128/8);
-    }
 });
+
+//*** Wrapper ***//
+
+var encryption = function(message, key) {
+	var salt = CryptoJS.lib.WordArray.random(512/8);
+	var iv = CryptoJS.lib.WordArray.random(512/8);
+	var key512Bits1000Iterations = CryptoJS.PBKDF2(key, salt, { keySize: 512/32, iterations: 1000 });
+	var encrypted = CryptoJS.AES.encrypt('test',key512Bits1000Iterations , { iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 });
+	var decrypted = CryptoJS.AES.decrypt(encrypted,key512Bits1000Iterations , { iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 });
+	var resultString = decrypted.toString(CryptoJS.enc.Utf8);
+	var result = {
+		salt: salt,
+		iv: iv,
+		enc: encrypted
+	}
+	return result;
+}
+var decryption = function(enc, key, salt, iv) {
+    var salt = CryptoJS.enc.Hex.parse(salt);
+    var iv = CryptoJS.enc.Hex.parse(iv);
+    var key512Bits1000Iterations = CryptoJS.PBKDF2(key, salt, { keySize: 512/32, iterations: 1000 });
+    var decrypted = CryptoJS.AES.decrypt(enc,key512Bits1000Iterations , { iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 });
+	return decrypted.toString(CryptoJS.enc.Utf8);
+}
+var hash1 = function(message) {
+	return CryptoJS.SHA256("Message");
+}
+var hash2 = function(message) {
+	return CryptoJS.SHA512("Message");
+}
+var validateHash = function(message, expected) {
+	
+}
+var generateRandom = function() {
+	return CryptoJS.lib.WordArray.random(128/8);
+}
